133b8f77acbf6df6104243f4925e7a660cab1743
Rosin <crosin@nist.gov>
2015-12-01 10:55:25 -0500

    first commit - MSH-21 conformance statements plugins

---
+++ src/main/java/gov/nist/healthcare/mu/ss/custom/SS_017_NO_ACK.java
@@ -0,0 +1,58 @@
+package gov.nist.healthcare.mu.ss.custom;
+
+import hl7.v2.instance.Element;
+import hl7.v2.instance.Query;
+import hl7.v2.instance.Simple;
+import org.apache.log4j.Logger;
+import scala.collection.Iterator;
+import scala.collection.immutable.List;
+
+public class SS_017_NO_ACK {
+
+    private String MSH21_1 = "PH_SS-NoAck";
+    private String MSH21_2 = "SS Sender";
+    private String MSH21_3 = "2.16.840.1.114222.4.10.3";
+    private String MSH21_4 = "ISO";
+
+    private static Logger logger = Logger.getLogger(SS_017_NO_ACK.class.getName());
+
+    public boolean assertion(hl7.v2.instance.Element e) {
+        List<Element> MSH21_List = Query.query(e, "1[1].21[*]").get();
+        Iterator<Element> it = MSH21_List.iterator();
+        while (it.hasNext()) {
+            Element MSH21 = it.next();
+            List<Simple> MSH21_1_List = Query.queryAsSimple(MSH21, "1[1]").get();
+            List<Simple> MSH21_2_List = Query.queryAsSimple(MSH21, "2[1]").get();
+            List<Simple> MSH21_3_List = Query.queryAsSimple(MSH21, "3[1]").get();
+            List<Simple> MSH21_4_List = Query.queryAsSimple(MSH21, "4[1]").get();
+
+            if (MSH21_1_List.size() != 1) {
+                logger.debug("MSH-21.1 list size = " + MSH21_1_List.size());
+                return false;
+            }
+            if (MSH21_2_List.size() != 1) {
+                logger.debug("MSH-21.2 list size = " + MSH21_2_List.size());
+                return false;
+            }
+            if (MSH21_3_List.size() != 1) {
+                logger.debug("MSH-21.3 list size = " + MSH21_3_List.size());
+                return false;
+            }
+            if (MSH21_4_List.size() != 1) {
+                logger.debug("MSH-21.4 list size = " + MSH21_4_List.size());
+                return false;
+            }
+
+            boolean check = MSH21_1.equals(MSH21_1_List.apply(0).value().raw())
+                    && MSH21_2.equals(MSH21_2_List.apply(0).value().raw())
+                    && MSH21_3.equals(MSH21_3_List.apply(0).value().raw())
+                    && MSH21_4.equals(MSH21_4_List.apply(0).value().raw());
+
+            if (check) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
